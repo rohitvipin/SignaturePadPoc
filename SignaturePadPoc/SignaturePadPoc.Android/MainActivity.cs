fc911c773f9c877e67c7ab1ee2d58faee0a019bc
Hina <hina.ahuja@cesltd.com>
2017-06-20 23:19:12 +0530

    Added Splash screen for android

--- SignaturePadPoc/SignaturePadPoc.Android/MainActivity.cs
+++ SignaturePadPoc/SignaturePadPoc.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace SignaturePadPoc.Droid
 {
-    [Activity(Label = "SignaturePadPoc", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SignaturePadPoc", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
